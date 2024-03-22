5e3e068d25dbb3cf5a2b4b4bdbd9c070b9c13644
Leo Pottier <leo.pottier1@outlook.fr>
2024-03-22 12:16:39 +0100

    deploy

--- src/Router.jsx
+++ src/Router.jsx
@@ -11,7 +11,7 @@ function Router() {
           <BrowserRouter basename="/Portfolio">
               <Header />
               <Routes>
-                  <Route exact path="/Portfolio" element={<Home />} />   
+                  <Route exact path="/" element={<Home />} />   
               </Routes>
               <Footer />
           </BrowserRouter>
